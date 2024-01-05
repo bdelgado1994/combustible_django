418e8ffd02e4d14c227a2385a6f2a452d5c73377
bdelgado1994 <59299188+bdelgado1994@users.noreply.github.com>
2024-01-04 22:53:48 -0600

    usermodel added

--- core/settings.py
+++ core/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "bases",
+    "user"
 ]
 
 MIDDLEWARE = [
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        "NAME": "dj_ctl_comb",
+        "USER": "postgres",
+        "PASSWORD": "",
+        "HOST": "db",
+        "PORT": "5432",
     }
 }
 
@@ -122,3 +127,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Para customUser
+AUTH_USER_MODEL="user.User"
