0ee1fb3aef4bc693c190b17751f9f664dc55621c
bdelgado1994 <59299188+bdelgado1994@users.noreply.github.com>
2024-03-04 23:09:47 -0600

    datatable added

--- ctrl_comb/models.py
+++ ctrl_comb/models.py
@@ -11,3 +11,19 @@ class Brand(models.Model):
         verbose_name="Marca"
         verbose_name_plural="Marcas"
 
+class Models(models.Model):
+    description=models.CharField(
+        max_length=50,
+        db_comment="Descripcion del modelo de Vehiculo"
+    )
+    brand=models.ForeignKey(
+        Brand,
+        on_delete=models.PROTECT,
+        )
+    def __str__(self):
+        return f"{self.brand} - {self.description}"
+    class Meta:
+        db_table="model"
+        db_table_comment="Modelos de Vehiculos"
+        verbose_name="Modelos"
+        verbose_name_plural="Modelos"
